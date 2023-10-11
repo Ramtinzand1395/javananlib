f81dd4ce2a728f0eec2cfbcd83ba767084ec6d43
ramtin.zand <ramtinzand6@gmail.com>
2023-10-11 19:08:51 +0330

    apiRoutes

--- Frontend/src/services/authorServices.js
+++ Frontend/src/services/authorServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:5000/api";
+const SERVER_URL = "library-three-sigma.vercel.app/api";
 
 // @desc  Update Author
 // @route PUT http://localhost:5000/api/update-author
